b3e4535cc7b4af0d1d0159c1e565dd50c1007871
Keith A. Lewis <kal@kalx.net>
2021-07-08 12:29:47 -0400

    correlation fixes blas arg naming

--- fms_correlation.h
+++ fms_correlation.h
@@ -1,10 +1,11 @@
 // fms_correlation.h - correlation matrix
 #pragma once
+#include <valarray>
 #include "fms_blas/fms_lapack.h"
 
 namespace fms {
 
-	static inline const char fms_correlation_doc[] = R"xyzyx(
+	static inline const char correlation_doc[] = R"xyzyx(
 Unit vectors \((e_i)\) determine a correlation matrix \([\rho_{ij}]\)
 where \(\rho_{ij} = e_i\cdot e_j\) is the inner product of the vectors.
 For every correlation matrix we can find unit vectors that satisfy this
@@ -15,63 +16,95 @@ of the lower decompsition are the \((e_i\).
 	class correlation : public blas::matrix<X, CblasNoTrans, CblasLower> {
 		std::valarray<X> _e; // lower Cholesky factor
 	public:
-		static inline const char fms_allocation_correlation_correlation_doc[] = R"xyzyx(
+		using blas::matrix<X, CblasNoTrans, CblasLower>::operator();
+		using blas::matrix<X, CblasNoTrans, CblasLower>::resize;
+		using blas::matrix<X, CblasNoTrans, CblasLower>::row;
+		using blas::matrix<X, CblasNoTrans, CblasLower>::rows;
+		using blas::matrix<X, CblasNoTrans, CblasLower>::transpose;
+
+		static inline const char doc[] = R"xyzyx(
 		Packed rows of the correlation matrix without the unit diagonal.
 )xyzyx";
-		static inline const char* fms_allocation_correlation_correlation_arguments[] = {
+		static inline const char* arguments[] = {
 			"is the dimension of the correlation matrix",
 			"is a pointer to an array of n*(n-1)/2 packed correlations",
 		};
-		correlation(size_t n, const X* rho)
-			: blas::matrix<X>(n, n), _e(n* n)
+		correlation(int n, const X* rho)
+			: blas::matrix<X, CblasNoTrans, CblasLower>(n, n), _e(n * n)
 		{
-			using blas::matrix<X>::operator();
+			resize(n, n, &_e[0]);
 
-			blas::matrix<X>::data() = &_e[0];
-
-			for (int i = 0; i < n; ++i) {
-				for (int j = 0; j < i; ++j) {
-					operator()(i, j) = rho[j + (i * (i + 1)) / 2];
-				}
+			operator()(0, 0) = X(1);
+			for (int i = 1; i < n; ++i) {
 				operator()(i, i) = X(1);
+				for (int j = 0; j < i; ++j) {
+					operator()(i, j) = rho[(i*(i - 1))/2 + j];
+				}
 			}
 
 			lapack::potrf(*this);
 		}
-		// i-th unit correlation vector
-		blas::vector<X> row(int i) const
-		{
-			return blas::matrix<X>::row(i);
+		correlation(const correlation& rho)
+			: blas::matrix<X, CblasNoTrans, CblasLower>(rho.rows(), rho.columns()), _e(rho.size())
+		{ 
+			_e = rho._e;
+			resize(rho.rows(), rho.columns(), &_e[0]);
 		}
+		correlation& operator=(const correlation& rho)
+		{
+			if (this != &rho) {
+				_e = rho._e;
+				resize(rho.rows(), rho.columns(), &_e[0]);
+			}
+
+			return *this;
+		}
+		~correlation()
+		{ }
+
+		int dimension() const
+		{
+			return rows();
+		}
+
+		// underlying correlation matrix
+		void get(X* cor) const
+		{
+			blas::gemm(*this, transpose(), cor);
+		}
+
 		// get i, j correlation
 		X rho(int i, int j) const
 		{
 			return blas::dot(row(i), row(j));
 		}
-		// set i, j correlation to r_ij by rotating e_i
-		correlation& rho(int i, int j, X rij)
+		// set i, j correlation to r_ij by rotating e_i along e_j
+		correlation& rho(int i, int j, X r_ij)
 		{
-			static constexpr X eps = sqrt(std::numeric_limits<X>::epsilon());
+			static X eps = sqrt(std::numeric_limits<X>::epsilon());
 
-			// so we can rotate back from rho = 0 or rho = 1
-			if (fabs(rij) < eps) {
-				rij = eps; // ??? sign, jitter
+			if (i == j) {
+				return *this;
 			}
-			else if (fabs(1 - rij) < eps) {
-				rij = 1 - eps;
+			// so we can rotate back from rho = 0 or rho = 1
+			if (fabs(r_ij) < eps) {
+				r_ij = eps; // ??? sign, jitter
+			}
+			else if (fabs(1 - r_ij) < eps) {
+				r_ij = 1 - eps;
 			}
 
 			// e_i' = c e_i + s e_j
-			// rij = e_i' . e_j = c rho + sqrt(1 - c^2)
+			// r_ij = e_i' . e_j = c rho + sqrt(1 - c^2)
 			// (rij - c rho)^2 = 1 - c^2
-			// (1 - rho^2)c^2 + 2 rij rho + 1 - rij^2 = 0
-			X rho = rho(i, j);
-			X A = 1 - rho * rho;
-			X B = rij * rho;
-			X C = 1 - rij * rij;
+			// (1 + rho^2)c^2 - 2 rij rho + rij^2 - 1 = 0
+			X rho_ij = rho(i, j);
+			X A = 1 + rho_ij * rho_ij;
+			X B = - r_ij * rho_ij;
+			X C = r_ij * r_ij - 1;
 			X D = B * B - A * C;
-			// largest c gives smallest theta
-			X c = fabs(A) > eps ? (-B + copysigne(sqrt(D), A)) / A : C / (B + copysign(sqrt(D), B));
+			// largest cos gives smallest theta
+			X c = fabs(A) > eps ? (-B + copysign(sqrt(D), A)) / A : C / (B + copysign(sqrt(D), B));
 
 			blas::scal<X>(c, row(i));
 			blas::axpy<X>(sqrt(1 - c * c), row(j), row(i));
---
+++ xll_correlation.cpp
@@ -0,0 +1,100 @@
+// xll_correlation.cpp - matrix correlation
+#include "fms_correlation.h"
+#include "xll/xll/xll.h"
+
+#define CATEGORY "XLL"
+
+using namespace fms;
+using namespace xll;
+
+AddIn xai_correlation_(
+	Function(XLL_HANDLEX, "xll_correlation_", "\\XLL.CORRELATION")
+	.Arguments({
+		Arg(XLL_LONG, "n", correlation<>::arguments[0]),
+		Arg(XLL_FPX, "rho", correlation<>::arguments[1]),
+		})
+	.Uncalced()
+	.FunctionHelp(correlation<>::doc)
+	.Category(CATEGORY)
+	.Documentation(correlation_doc)
+);
+HANDLEX WINAPI xll_correlation_(LONG n, const _FPX* prho)
+{
+#pragma XLLEXPORT
+	HANDLEX h = INVALID_HANDLEX;
+
+	try {
+		handle<correlation<>> h_(new correlation(n, prho->array));
+		h = h_.get();
+	}
+	catch (const std::exception& ex) {
+		XLL_ERROR(ex.what());
+	}
+
+	return h;
+}
+
+AddIn xai_correlation(
+	Function(XLL_FPX, "xll_correlation", "XLL.CORRELATION")
+	.Arguments({
+		Arg(XLL_HANDLEX, "cor", "is a handle returned by \\XLL.CORRELATION."),
+		})
+	.FunctionHelp("Return the correlation matrix.")
+	.Category(CATEGORY)
+	.Documentation(R"()")
+);
+_FPX* WINAPI xll_correlation(HANDLEX hrho)
+{
+#pragma XLLEXPORT
+	static FPX cor;
+
+	try {
+		handle<correlation<>> h(hrho);
+		ensure(h);
+		cor.resize(h->rows(), h->columns());
+		h->get(cor.array());
+	}
+	catch (const std::exception& ex) {
+		XLL_ERROR(ex.what());
+
+		return nullptr;
+	}
+
+	return cor.get();
+}
+
+AddIn xai_correlation_rho(
+	Function(XLL_HANDLEX, "xll_correlation_rho", "XLL.CORRELATION.RHO")
+	.Arguments({
+		Arg(XLL_HANDLEX, "cor", "is a handle returned by \\XLL.CORRELATION."),
+		Arg(XLL_LONG, "i", "is the row index."),
+		Arg(XLL_LONG, "j", "is the column index."),
+		Arg(XLL_LPOPER, "_rho", "is a optional correlation to set."),
+		})
+		.FunctionHelp("Return the correlation or matrix handle if _rho is not missing.")
+	.Category(CATEGORY)
+	.Documentation(R"()")
+);
+HANDLEX WINAPI xll_correlation_rho(HANDLEX hrho, LONG i, LONG j, LPOPER prho)
+{
+#pragma XLLEXPORT
+	HANDLEX h = INVALID_HANDLEX;
+
+	try {
+		handle<correlation<>> h_(hrho);
+		ensure(h_);
+		if (prho->is_missing()) {
+			h = h_->rho(i, j);
+		}
+		else {
+			ensure(prho->is_num());
+			h_->rho(i, j, prho->as_num());
+			h = hrho;
+		}
+	}
+	catch (const std::exception& ex) {
+		XLL_ERROR(ex.what());
+	}
+
+	return h;
+}
