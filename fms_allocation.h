5f6deab2d15bcbb87bc22b4f4fba9328ea63ece7
Keith A. Lewis <keithalewis@live.com>
2021-10-19 10:53:30 -0400

    maximize return given volatiltiy

--- fms_allocation.h
+++ fms_allocation.h
@@ -3,6 +3,7 @@
 #pragma once
 #include <valarray>
 #include "fms_blas/fms_lapack.h"
+#include "fms_blas/fms_trnlsp.h"
 #include "fms_correlation.h"
 
 namespace fms::allocation {
@@ -16,14 +17,13 @@ and a target expected realized return \(\rho\), find a portfolio having miniumum
 	class portfolio {
 		blas::vector_alloc<X> V_x, V_EX; // V^-1 x, V^-1 E[R]
 		X A, B, C, D;
+
+
 		void ABCD(const blas::vector<X>& EX)
 		{
-			double _1 = 1;
-			blas::vector x(EX.size(), &_1, 0); // x = {1,1, ...}
-
-			A = blas::dot(x, V_x);   // x . V_x
-			B = blas::dot(x, V_EX);  // x . V_EX
-			C = blas::dot(EX, V_EX); // E[X] . V_EX
+			A = sum(V_x);   // x . V_x
+			B = sum(V_EX);  // x . V_EX
+			C = blas::dot(EX, V_EX); // E[X]' V^{-1} EX
 			D = A * C - B * B;
 		}
 	public:
@@ -73,14 +73,29 @@ and a target expected realized return \(\rho\), find a portfolio having miniumum
 			return V_x.size();
 		}
 
+		X lambda(double r) const
+		{
+			return (C - r * B) / D;
+		}
+		X mu(double r) const
+		{
+			return (r * A - B) / D;
+		}
+
 		// minimize variance given target return
 		// optimal porfolio is put in xi
 		// minimum variance is returned
 		// xi = V^-1(lambda x + mu E[X])
-		X minimize(X r, X* _xi)
+		X minimize(X r, X* _xi, double* _lambda = nullptr, double* _mu = nullptr)
 		{
 			X lambda = (C - r * B) / D;
 			X mu = (r * A - B) / D;
+			if (_lambda) {
+				*_lambda = lambda;
+			}
+			if (_mu) {
+				*_mu = mu;
+			}
 
 			if (_xi) {
 				// xi = lambda V_x + mu V_EX
@@ -105,7 +120,7 @@ and a target expected realized return \(\rho\), find a portfolio having miniumum
 		// 0 = (C - sigma^2 B^2) - 2(B - sigma^2 AB) lambda + (A - sigma^2 A^2) lambda^2
 		// mu = B - lambda A
 		// xi.EX = (C - lambda B)/mu 
-		X maximize(X sigma, X* _xi)
+		X maximize(X sigma, X* _xi, double* _lambda = nullptr, double* _mu = nullptr)
 		{
 			double c = C - sigma * sigma * B * B;
 			double b = B - sigma * sigma * A * B;
@@ -114,6 +129,12 @@ and a target expected realized return \(\rho\), find a portfolio having miniumum
 
 			double lambda = (-b + d) / a; // +- d ???
 			double mu = B - lambda * A;
+			if (_lambda) {
+				*_lambda = lambda;
+			}
+			if (_mu) {
+				*_mu = mu;
+			}
 
 			if (_xi) {
 				// xi = (V_EX - lambda V_X)/mu
@@ -125,7 +146,71 @@ and a target expected realized return \(\rho\), find a portfolio having miniumum
 
 			return (C - lambda * B) / mu;
 		}
+
 	};
 
+	// x = (lambda, um, xi)
+	template<class X>
+	X maximize(X sigma, int n, const double* ER, const double* V, double* x)
+	{
+		portfolio<X> p(n, ER, V);
+		
+		return p.maximize(sigma, x + 2, x + 0, x + 1);
+	}
+	// F(lambda, mu, xi) = -(xi.EX - lambda(xi.x - 1) - mu/2 (xi' V xi - sigma^2))
+	// subject to l <= xi <= u
+	// D_lambda = xi.x - 1, x = (1, 1, ...)
+	// D_mu = V xi - sigma*sigma
+	// D_xi F(lambda, mu, xi) = -EX + lambda x + mu V xi
+	// x = (lambda, mu, xi)
+	// On entry x contains initial guess
+	template<class X>
+	X maximize(X sigma, int n, const double* ER, const double* V, double* xi, const double* l, const double* u)
+	{
+		// initial xi
+		maximize(sigma, n - 2, ER, V, xi);
+		fms::trnslpbc p(n, 1, xi, l, u);
+		p.f = [&n,sigma,ER,V](int n, int, int, const double* x, double* fx) {
+			blas::matrix v(n, n, V);
+			double lambda = x[0];
+			double mu = x[1];
+			blas::vector xi(n, x + 2);
+			*fx = -blas::dot(xi, blas::vector(n, ER));
+			*fx += lambda * (blas::sum(xi) - 1);
+			*fx += (mu / 2) * (blas::quad(CblasLower, v, xi) - sigma & sigma);
+		};
+		p.df = [&n, sigma, ER, V](int n, int, int, const double* x, double* df) {
+			blas::matrix v(n, n, V);
+			double lambda = x[0];
+			double mu = x[1];
+			blas::vector xi(n, x + 2);
+			df[0] = blas::sum(xi) - 1;
+			df[1] = (blas::quad(CblasLower, v, xi) - sigma * sigma) / 2;
+			blas::vector dF(n, df + 2);
+			// mu V xi
+			blas::gemv(v, xi, dF.data(), dF.incr(), mu);
+			// + lambda x
+			blas::axpy(1, blas::vector(n, &lambda, 0), dF);
+			// -EX
+			blas::axpy(-1, blas::vector(n, ER), dF);
+		};
+		ensure(TR_SUCCESS == p.init());
+
+		blas::vector<X> x(2 + n, xi);
+		blas::vector_alloc<X> df(2 + n);
+		ensure(TR_SUCCESS = p.check(x.data(), df.data()));
+		
+		int rci = 0;
+		ensure(TR_SUCCESS == p.solver(x.data(), df.data(), rci));
+
+		int iter = 0, st_cr = 0;
+		double r1 = 0, r2 = 0;
+		ensure(TR_SUCCESS == p.get(iter, st_cr, r1, r2));
+
+		double result;
+		p.f(n, 1, x, &result);
+
+		return result;
+	}
 
 } // namespace fms::allocation
